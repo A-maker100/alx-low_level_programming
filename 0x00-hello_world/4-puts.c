643bfd1b31239273d0d2977becf261d143914b11
Apinda <apinda217@gmail.com>
2023-02-16 14:48:36 -0800

    Whoa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,15 @@
 
 /**
  * main function puts output to the terminal
-
+ * "Programming is like building a mutlilingual puzzle
  */
 
 int main(void)
 {
 
 puts("\"Programming is like building a mutlilingual puzzle");
-/*Returns 0 after success*/
+/**
+ * Returns 0 after success 
+*/
 return (0);
 }
