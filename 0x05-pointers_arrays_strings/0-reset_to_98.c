9a25605fed2ac33e1f68850a96dce0faedf9b6bf
Apinda <apinda217@gmail.com>
2023-02-27 04:00:24 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void reset_to_98(int *n);
+
+/**
+ * main - Major function of the program
+ * @void : main function takes no parameters
+ * Description : The function will call another one to reset things to zero
+ * Return: Always returns 0 at the end of function
+ */
+
+int main(void)
+{
+	int number;
+	int *p;
+
+	number = 420;
+	p = &number;
+	reset_to_98(p);
+	return (0);
+}
+/**
+ * reset_to_98 - Additional function in the program
+ * @n : the function takes n as a parameter
+ * Description : resets the value in n to 98
+ * Return: no return value; void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x05-pointers_arrays_strings
+This README.md was created on the 27 Feb 2023
