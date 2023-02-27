ddf70956ad09ada5c4294efb24f693c7e19f02b9
Apinda <apinda217@gmail.com>
2023-02-27 15:15:24 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+/**
+ * rev_string- main function in this program
+ * @s : char pointer as a parameter string
+ * Description : this function reverses a string but it does not print it
+ * Return: always 0
+ */
+#include<stdio.h>
+#include<string.h>
+void rev_string(char *s)
+{
+	int length;
+	int c;
+	char temp;
+	
+	length = strlen(s);
+	for (c = 0; c < length; c++)
+	{
+		temp = s[c];
+		s[c] = s[length - (c + 1)];
+		s[length - (c + 1)] = temp;
+	}
+
+}
+
+int main(void)
+{
+	char *t = "welcome to my world";
+	rev_string(t);
+	printf("%s\n", t);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+/**
+ * puts2- Major function that will be in this program
+ * @str: character pointer that is a string
+ * Description : prints every other character of a string
+ * Return: 0
+ */
+#include<stdio.h>
+#include<string.h>
+void puts2(char *str)
+{
+	int length;
+	int index;
+
+	length = strlen(str);
+	for (index =  0; index < length; index += 2)
+	{
+		putchar(str[index]);
+	}
+}
+
+int main(void)
+{
+	char *s = "This is interestingly interesting\n";
+	puts2(s);
+
+	return (0);
+}
