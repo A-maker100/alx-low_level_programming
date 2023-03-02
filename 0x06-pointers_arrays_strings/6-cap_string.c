3e4cff635f9a276ab51b6da35aca06694ee87646
Apinda <apinda217@gmail.com>
2023-03-02 04:57:47 -0800

    6-cap_string...updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,48 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
+#include<stdbool.h>
 char *cap_string(char *c)
 {
-	int MARKS[13] = {32, 9, 10, 44, 59, 46, 33, 64, 34, 40, 41, 123, 125};
+	char MARKS[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int length, index, marks_index;
 	int ch;
+	bool flag;
 
 	length = strlen(c);
-	c[0] = toupper(c[0]);
+	/* Ensure the first character is a letter or number */
+	if (isalpha(c[0]))
+	{
+		c[0] = toupper(c[0]);
+		flag = true;
+	}
+
 	for (index = 1; index < length; index++)
 	{
 		ch = c[index];
 		for (marks_index = 0; marks_index < 13; marks_index++)
 		{
-			if (ch == MARKS[marks_index])
+			while (!isalnum(ch) && ch == MARKS[marks_index])
 			{
-				ch = c[++index];
-				c[index] = toupper(c[index]);
+				flag = true;
+				++index;
+				ch = c[index];
+				if (isalnum(ch))
+				{
+					flag = false;
+				}
+				else
+				{
+					marks_index = 0;
+				}
 			}
 		}
+		if (flag == false && isalnum(ch))
+		{
+			c[index] = toupper(ch);
+			flag = true;
+		}
 	}
+
 	return (c);
 }
