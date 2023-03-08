13cd58a0a46159de1d4600a719736e069402f329
Apinda <apinda217@gmail.com>
2023-03-08 11:42:53 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+/**
+ *_puts_recursion - Major function in this program
+ * @s: string literal as parameter
+ * Description: prints a string to std output
+ * Return: void; no return value
+ */
+#include<string.h>
+#include<stdio.h>
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
