b5ba59ee176a298a7462a7413e7f98be629fbce9
Apinda <apinda217@gmail.com>
2023-03-08 13:53:38 -0800

    4-pow_recursion.c ...updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,26 @@
 #include<string.h>
 #include<stdio.h>
 /**
+ * _rev_recursion-Function that prints a string
+ * @s: original string, first parameter
+ * @index: index of a char of a string
+ * Description: prints a string in reverse using recursion
+ * Return: void
+ */
+void _rev_recursion(char *s, int index)
+{
+	int INDEX, length;
+
+	length = strlen(s);
+	if (index == length)
+	{
+		return;
+	}
+	INDEX = index;
+	_rev_recursion(s, ++index);
+	putchar(s[INDEX]);
+}
+/**
  * _print_rev_recursion-Major function in this program
  * @s: parameter string to be reversed
  * Description: prints an input string in reverse
@@ -8,13 +28,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	puts(strrev(s));
-}
+	int IND;
 
-int main(void)
-{
-	char tree[] = "Tree of life";
-	_print_rev_recursion(tree);
-
-	return(0);
+	IND = 0;
+	_rev_recursion(s, IND);
+	putchar(10);
 }
