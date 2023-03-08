d0cf8013d2a8d6f14849f1b86e5d66d9ecbeaab0
Apinda <apinda217@gmail.com>
2023-03-08 13:00:33 -0800

    2-strlen_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,26 +1,6 @@
 #include<string.h>
 #include<stdio.h>
 /**
- * _rev_recursion-Function that prints a string
- * @s: original string, first parameter
- * @index: index of a char of a string
- * Description: prints a string in reverse using recursion
- * Return: void
- */
-void _rev_recursion(char *s, int index)
-{
-	int INDEX, length;
-
-	length = strlen(s);
-	if (index == length)
-	{
-		return;
-	}
-	INDEX = index;
-	_rev_recursion(s, ++index);
-	putchar(s[INDEX]);
-}
-/**
  * _print_rev_recursion-Major function in this program
  * @s: parameter string to be reversed
  * Description: prints an input string in reverse
@@ -28,9 +8,13 @@ void _rev_recursion(char *s, int index)
  */
 void _print_rev_recursion(char *s)
 {
-	int IND;
+	puts(strrev(s));
+}
 
-	IND = 0;
-	_rev_recursion(s, IND);
-	putchar(10);
+int main(void)
+{
+	char tree[] = "Tree of life";
+	_print_rev_recursion(tree);
+
+	return(0);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion- Major function in this program
+ * @s: input string parameter
+ * Description: returns length of input string
+ * Return: int
+ */
+#include<string.h>
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
