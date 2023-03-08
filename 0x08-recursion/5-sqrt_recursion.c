be87aa1ad7e749e1adb52e1de69bc2cb62a9b807
Apinda <apinda217@gmail.com>
2023-03-08 14:52:14 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _sqrt_recursion- Major function in the program
+ * @n: integer parameter
+ * Description: Function returns sqrt of n
+ * Return: -1 if sqrt is NaN or official sqrt
+ */
+#include<math.h>
+#include<stdio.h>
+int _sqrt_recursion(int n)
+{
+	return (0);
+}
+
+int main(void)
+{
+	int NUMBER = -18, squrt;
+	squrt = sqrt(16);
+
+	printf("%d\n", squrt);
+	_sqrt_recursion(NUMBER);
+	return (0);
+
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,52 @@
+
+/**
+ *is_prime- Helper function in the program
+ * @n: The original number
+ * @index: the divisor
+ * Description: recursive division function
+ * Return: true if prime; false otherwise
+ */
+bool is_prime(int n, int index)
+{
+	if (index == n)
+	{
+		return (true);
+	}
+
+	else if (n % index == 0)
+	{
+		return (false);
+	}
+
+	return (is_prime(n, ++index));
+}
+
+/**
+ * _is_prime_number - Major function in this program
+ * @n: integer parameter
+ * Description: determines if a number is prime number or not
+ * Return: 0 if not, 1 if it is
+ */
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int _is_prime_number(int n)
+{
+	int IND;
+
+	IND = 2;
+	if (n == 1 || n == 0)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	if (is_prime(n, IND))
+	{
+		return (1);
+	}
+	return (0);
+}
