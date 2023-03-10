323ba89cfb31a587b696a8513a35c84eb6fbcf0c
Apinda <apinda217@gmail.com>
2023-03-10 11:02:46 -0800

    name output using Command-Line(CL) argz

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-main function
+ * @argc: counts number of arguments when program is being run
+ * @argv: an array of CL args when program is being run
+ * Description: uses argv to print out program name
+ * Return: always 0
+ */
+int main( int argc, char *argv[])
+{
+	char *program_name;
+
+	program_name = argv[0];
+	/* program name will always be index 0 */
+	printf("%s\n", program_name);
+	return (0);
+}
