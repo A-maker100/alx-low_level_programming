be7383683bd104995091c86fba3b46b93b6fcb30
Apinda <apinda217@gmail.com>
2023-03-10 11:12:24 -0800

    print out program name...updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
-/#include<stdio.h>
+#include<stdio.h>
 /**
  * main-main function
  * @argc: counts number of arguments when program is being run
  * @argv: an array of CL args when program is being run
  * Description: uses argv to print out program name
- * Return: always 0
+ * Return: 0 for success and 1 for errors
  */
 int main(int argc, char *argv[])
 {
 	char *program_name;
 
-	program_name = argv[0];
+	program_name = argv[argc - 1];
 	/* program name will always be index 0 */
 	printf("%s\n", program_name);
 	return (0);
